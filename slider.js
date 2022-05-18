f394a35baeebf66e9a1b7d1da82d605ce3dce793
Truowng <quockhanhtruong166@gmail.com>
2022-05-19 03:10:27 +0700

    done!

---
+++ slider.js
@@ -0,0 +1,44 @@
+const slides = document.getElementsByClassName("slide");
+const slideCount = slides.length;
+
+let i = 0;
+function next() {
+  i++;
+  if (i < slideCount) {
+    slides[i].style.left = "0";
+    slides[i - 1].style.left = "-100%";
+    console.log("advanced i = " + i);
+  } else {
+    i = 0; // Set current slide back to zero
+    slides[i].style.left = "0";
+    slides[slideCount - 1].style.left = "-100%";
+
+    console.log("back to start");
+
+    for (let x = 1; x < slideCount - 1; x++) {
+      slides[x].style.left = "100%";
+    }
+  }
+}
+document.getElementById("next").onclick = next;
+
+function prev() {
+  i--;
+  if (i >= 0) {
+    slides[i].style.left = "0";
+    slides[i + 1].style.left = "-100%";
+    console.log("advanced i = " + i);
+  } else {
+    i = slideCount - 1; // Set current slide back to zero
+    slides[i].style.left = "0";
+    slides[0].style.left = "-100%";
+
+    console.log("back to last" + i);
+
+    for (let x = slideCount - 1; x > 0; x--) {
+      slides[x].style.right = "100%";
+    }
+  }
+}
+
+document.getElementById("previous").onclick = prev;
